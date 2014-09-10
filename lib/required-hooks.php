923ddfd77f41c8051606c2e8acfa03e7056d9376
Peith Vergil <peith.vergil@gmail.com>
2014-09-10 08:47:32 +0800

    Display the product author's name below the product price.

--- lib/required-hooks.php
+++ lib/required-hooks.php
@@ -17,10 +17,25 @@ function ite_multi_author_template_paths( $paths=array() ) {
 add_filter( 'it_exchange_possible_template_paths', 'ite_multi_author_template_paths' );
 
 
+/**
+ * Display the product author in the product detail page.
+ *
+ * @param array $parts The product information to display.
+ *
+ * @return array
+ */
 function ite_multi_author_product_author( $parts ) {
-    $parts[] = 'ibd-multi-author-store';
+    $new_parts = array();
 
-    return $parts;
+    foreach ( $parts as $part ) {
+        $new_parts[] = $part;
+
+        // Insert the author info just below the price info.
+        if ( $part === 'base-price' )
+            $new_parts[] = 'ibd-multi-author-store';
+    }
+
+    return $new_parts;
 }
 
 add_filter( 'it_exchange_get_content_product_product_info_loop_elements', 'ite_multi_author_product_author' );
