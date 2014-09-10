2abbad555d7632147439eb3eb18d90c962e05f6a
Peith Vergil <peith.vergil@gmail.com>
2014-09-10 08:48:19 +0800

    Added the WP string translation function. A CSS class was also added to the <div> element.

--- lib/templates/content-product/elements/ibd-multi-author-store.php
+++ lib/templates/content-product/elements/ibd-multi-author-store.php
@@ -4,7 +4,9 @@ $product = it_exchange_get_product( $GLOBALS['it_exchange']['product']->ID );
 $author = get_userdata( $product->post_author );
 
 ?>
-<div>
-    Author: <?php printf('%s %s', $author->first_name, $author->last_name); ?>
+<div class="it-exchange-product-author">
+    <p>
+        <?php printf( __( 'Author: %s %s', 'ibd_multi_author' ), $author->first_name, $author->last_name ); ?>
+    </p>
 </div>
 
