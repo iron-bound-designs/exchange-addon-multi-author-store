b2bc8acabf319de360715683d4d3ecc1cf1e9b2e
Timothy Jacobs <timothy@ironbounddesigns.com>
2014-09-11 23:14:02 -0400

    Use the product feature functions to grab the author ID

--- lib/templates/content-product/elements/ibd-multi-author-store.php
+++ lib/templates/content-product/elements/ibd-multi-author-store.php
@@ -1,12 +1,12 @@
 <?php
-$product = it_exchange_get_product( $GLOBALS['it_exchange']['product']->ID );
-$author = get_userdata( $product->post_author );
+$author  = get_userdata( it_exchange_get_product_feature( $GLOBALS['it_exchange']['product']->ID , 'ibd-multi-author-store' ) );
 ?>
+
 <?php do_action( 'it_exchange_content_product_before_product_author_element' ); ?>
 <div class="it-exchange-product-author">
-    <p>
-        <?php printf( __( 'Author: %s %s', ITE_Multi_Author::SLUG ), $author->first_name, $author->last_name ); ?>
-    </p>
+	<p>
+		<?php printf( __( 'Author: %s %s', ITE_Multi_Author::SLUG ), $author->first_name, $author->last_name ); ?>
+	</p>
 </div>
 <?php do_action( 'it_exchange_content_product_after_product_author_element' ); ?>
 
